a9369f794745a6dd02ea09431ba0b3d3ba270c9d
HariharanHarish2 <hariharanharish704@gmail.com>
2026-01-26 23:34:29 +0530

    Added folders and organized files

--- error handling/demo.py
+++ error handling/demo.py
@@ -4,6 +4,8 @@ content=f.read(10)#fun inside give 10 the give first ten vlue
 print(content)
 #print(f.readline())#line by line read
 f.close()
+
+
 name = input()
 mark =int (input())
 Department=input()
